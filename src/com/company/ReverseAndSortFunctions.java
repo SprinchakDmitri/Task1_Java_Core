635686f21471e87f8d387b5b405da236d3d7fc50
SprinchakDmitri <sprinchak.dmitriy@gmail.com>
2020-10-07 14:14:55 +0300

    Second commit

--- src/com/company/ReverseAndSortFunctions.java
+++ src/com/company/ReverseAndSortFunctions.java
@@ -1,16 +1,16 @@
 package com.company;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
 
 public class ReverseAndSortFunctions {
 
     public static void main(String[] args) {
-        int [] array = {1,17,16,15,61,2,521,17,16,15,39,234,156,154,153,140,13,18,190,
-                191,410,60,41,2,52,1555,123,512,5516,71,1,17,16,15,39,234,156,154,153,
-                140,13,18,190,191,410,60,41,2,52,234,3,123,513,1,253,272,274,23456,163,1346,2,62,623,26177};
+        int [] array = {1,17,16,15,61,2,13,1,163,1346,2,62,6};
 
-       sort(array);
-        System.out.print("Sorted array:");
+        sort(array);
+
+        System.out.println("Sorted array: ");
         for (int arrayElement: array)
         {
             System.out.print(arrayElement + " ");
@@ -26,18 +26,14 @@ public class ReverseAndSortFunctions {
         if (k <= array.length && k >=0) {
             int[] tempArray = new int[k];
 
-            for (int i=0, j=k-1; i < k ;i++, j--) tempArray[i] = array[j];
+            for (int i = 0, j = k-1; i < k ;i++, j--) tempArray[i] = array[j];
 
             for(int i = 0; i < k; i ++){
                 array[i] = tempArray[i];
-
             }
         }
         return array;
         }
-
-
-
 
         public static int[] sort(int[] array){                                  //sort method
         int k = 0;                        //reversing coefficient
--- src/com/company/SumOfDigits.java
+++ src/com/company/SumOfDigits.java
@@ -6,19 +6,25 @@ public class SumOfDigits {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int number = in.nextInt();
+        int number;
+        while(true){
+            System.out.println("Enter a number between 0 and 1000 \n" +
+                    "to count summ of all digits");
+            number = in.nextInt();
+            if (number > 0 && number < 1000) break;
+            System.out.println("Bad number. Try once again");
+        }
         System.out.println(getSumOfDigits(number));
 
     }
 
     public static int getSumOfDigits(int number) {
         int sumOfDigits = 0;
-        if (number > 0 && number < 1000) {
+
             for (int i = number; i > 0; i /= 10) {
                 sumOfDigits += i % 10;
             }
 
-        }
         return sumOfDigits;
     }
 
